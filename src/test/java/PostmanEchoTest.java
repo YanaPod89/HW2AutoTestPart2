524dd16eb9f7a74e3c0e98884d3fbf2e2603c426
Yana <podolskaya_22@mail.ru>
2024-09-20 23:19:17 +0700

    shouldNotPassCI

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 // Проверки
             .then()
             .statusCode(200)
-            .body("data", equalTo("yana"));
+            .body("data", equalTo("yana2"));
 
 }
 }
